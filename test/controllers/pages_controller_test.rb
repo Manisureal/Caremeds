d2843a98dbe409b6a7135cf9aa988af14a4cc392
Mansoor Zaman <manisureal@outlook.com>
2018-03-06 11:49:41 +0000

    Added a test for root route

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,6 +5,11 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     @new_title = "CareMeds"
   end
 
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get pages_home_url
     assert_response :success
