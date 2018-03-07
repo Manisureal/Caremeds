acb75be90f3df4b99a2eb5db775700bd73496695
Mansoor Zaman <manisureal@outlook.com>
2018-03-07 10:57:00 +0000

    Dropdown has now been fixed

---
+++ app/javascript/components/navbar.js
@@ -0,0 +1,10 @@
+// import 'jquery';
+
+function dropDown() {
+  $(document).ready(function(){
+      $('.dropdown-toggle').dropdown();
+  });
+}
+
+export { dropDown };
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,5 +7,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
-import 'bootstrap';
+
+// import 'jquery';
+// import 'bootstrap';
+console.log('Hello World from Webpacker');
+
+// import { dropDown } from '../components/navbar';
+// dropDown();
+
+console.log('Imported');
