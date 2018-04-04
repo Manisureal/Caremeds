c4786d41021b63dedfef69e548f393adc2be16dd
Mansoor Zaman <manisureal@outlook.com>
2018-04-04 21:36:16 +0100

    Email has correct formatting now

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,5 +1,7 @@
 class MessageMailer < ApplicationMailer
 
+#This should have been called UserMailer
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -8,7 +10,9 @@ class MessageMailer < ApplicationMailer
   def contact_me(contact)
     # @greeting = "Hi"
     @body = contact.body
+    @name = contact.name
+    @email = contact.email
 
-    mail to: "mansoor@caremeds.co.uk", from: contact.email
+    mail to: "mansoor@caremeds.co.uk", from: "#{@email}", subject: "Message from #{@name}"
   end
 end
