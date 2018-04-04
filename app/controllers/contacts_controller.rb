fde4b6aefb2f0c6cda49b44e9a5e64e4247623a3
Mansoor Zaman <manisureal@outlook.com>
2018-04-04 15:55:40 +0100

    Contact Us Form working now in production

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,9 +4,11 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(contact_params)
 
+    @contact = Contact.new(contact_params)
+    # break
     if @contact.valid?
+
       MessageMailer.contact_me(@contact).deliver_now
       redirect_to new_contact_url, notice: "Message received, Thanks!"
     else
