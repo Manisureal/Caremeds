18f65eed9aad8aa7b59b6aa35b81adce46d4c156
Mansoor Zaman <manisureal@outlook.com>
2018-05-10 14:05:10 +0100

    Shortened route link names

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
-  get 'pages/pharmacy'
-
-  get 'pages/careproviders'
-
-  get 'pages/faqs'
-
-  get 'pages/news'
-
-  get 'pages/contactus'
-
-  get 'pages/updates'
-
   root to: 'pages#home'
+
+  get 'home', to: 'pages#home', as: 'home'
+  get 'pharmacy', to: 'pages#pharmacy', as: 'pharmacy'
+  get 'careproviders', to: 'pages#careproviders', as: 'careproviders'
+  get 'faqs', to: 'pages#faqs', as: 'faqs'
+  get 'news', to: 'pages#news', as: 'news'
+  get 'contactus', to: 'pages#contactus', as: 'contactus'
+  get 'updates', to: 'pages#updates', as: 'updates'
 
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
   post 'contact-us', to: 'contacts#create', as: 'create_contact'
