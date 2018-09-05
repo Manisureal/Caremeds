0dea5f41d72f4957b575790de661d0a13a43da06
Mansoor Zaman <manisureal@outlook.com>
2018-09-05 12:12:04 +0100

    Create an action for the lead through api call to the CRM

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -25,7 +25,10 @@ class ContactsController < ApplicationController
       # arr.each_with_index {|value, index| contact_email_hash[index.to_s] = value}
       # @arr_with_hash << contact_email_hash
       # @api_client.post('contacts.json', {'last_name': @contact.name, 'emails': @arr_with_hash, 'background': @contact.body} )
-      @api_client.post('contacts.json', {'first_name': @contact.first_name, 'last_name': @contact.last_name, 'emails': [{ 'type' => 'work', 'value' => @contact.email}], 'phones': [{ 'type' => 'other', 'value' => @contact.phone}], 'background': @contact.body} )
+      @new_contact = @api_client.post('contacts.json', {'first_name': @contact.first_name, 'last_name': @contact.last_name, 'emails': [{ 'type' => 'work', 'value' => @contact.email}], 'phones': [{ 'type' => 'other', 'value' => @contact.phone}], 'background': @contact.body} )
+      @contact_id = @new_contact["data"]["contact"]["id"]
+      @new_action = @api_client.post('actions.json',
+                      "assignee_id": "","contact_id": @contact_id,"date": "","done": false,"id": "","modified_at": "","status": "date","text": "New Lead created through the CAREMEDS Website!" )
       redirect_to request.referrer, notice: "Thank you, We will be in touch shortly!"
     else
       render :new
