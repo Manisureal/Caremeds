d6a17f50aed7f7018070f09f549e09d2c1d5fcc1
Mansoor Zaman <manisureal@outlook.com>
2018-09-12 10:06:13 +0100

    Organization field added to contact us form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact
   include ActiveModel::Model
   # attr_accessor :name, :email, :body
-  attr_accessor :first_name, :last_name, :phone, :email, :body
-  validates :first_name, :last_name, :phone, :email, :body, presence: true
+  attr_accessor :first_name, :last_name, :phone, :email, :body, :organization
+  validates :first_name, :last_name, :phone, :email, :body, :organization, presence: true
 end
